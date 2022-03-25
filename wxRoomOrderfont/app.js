19c918f55d18818bbbe20a5f99756bd6c998d527
yjr-1100 <jiaruiyang221@gmail.com>
2022-03-25 20:39:14 +0800

    小程序前端基本完成

--- wxRoomOrderfont/app.js
+++ wxRoomOrderfont/app.js
@@ -2,7 +2,7 @@
  * @Author: YJR-1100
  * @Date: 2022-03-21 22:06:52
  * @LastEditors: YJR-1100
- * @LastEditTime: 2022-03-24 16:29:05
+ * @LastEditTime: 2022-03-25 20:36:35
  * @FilePath: \wx_RoomOrder\wxRoomOrderfont\app.js
  * @Description: 
  * @
@@ -15,27 +15,35 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
     // 登录
-    // wx.login({
-    //   success (res) {
-    //     if (res.code) {
-    //       //发起网络请求
-    //       console.log(res.code);
-    //       wx.request({
-    //         url: 'http://127.0.0.1:5000/api/v1/getopenid',
-    //         data: {
-    //           code: res.code
-    //         },
-    //         dataType:"json",
-    //         success(res){
-    //           var openid = res.data.openid
-    //           wx.setStorageSync('openid',openid)
-    //         }
-    //       })
-    //     } else {
-    //       console.log('登录失败！' + res.errMsg)
-    //     }
-    //   }
-    // })
+    wx.login({
+      success (res) {
+        if (res.code) {
+          //发起网络请求
+          console.log(res.code);
+          wx.request({
+            url: 'http://127.0.0.1:5000/api/v1/getopenid',
+            data: {
+              code: res.code
+            },
+            method:"GET",
+            dataType:"json",
+            success(res){
+              var openid = res.data.openid
+              wx.setStorageSync('openid',openid)
+            },
+            fail(e){
+              console.log(e)
+            }
+          })
+        } else {
+          wx.showToast({
+            title: '数据错误请重开',
+            icon: 'error',//
+            duration: 1000
+          })
+        }
+      }
+    })
   },
   globalData: {
     userInfo: null,
--- wxRoomOrderfont/pages/myorders/myorders.js
+++ wxRoomOrderfont/pages/myorders/myorders.js
@@ -1,3 +1,15 @@
+
+
+/**
+ * @Author: YJR-1100
+ * @Date: 2022-03-25 17:21:08
+ * @LastEditors: YJR-1100
+ * @LastEditTime: 2022-03-25 19:43:08
+ * @FilePath: \wx_RoomOrder\wxRoomOrderfont\pages\myorders\myorders.js
+ * @Description: 
+ * @
+ * @Copyright (c) 2022 by yjr-1100/CSU, All Rights Reserved. 
+ */
 // pages/myorders/myorders.js
 Page({
 
@@ -5,7 +17,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    /*
+    # -1 未审核
+    # 0  不通过
+    # 1  通过
+    */
+    orderlist:[{
+      status:-1,
+      roomname:"会议室",
+      usingtime:"2022-12-13 8:00-9:00",
+      ordertime:"2022-12-12 04:23",
+      address:"科教北501-2"
+    },
+    {
+      status:0,
+      roomname:"会议室",
+      usingtime:"2022-12-13 18:00-19:00",
+      ordertime:"2022-12-12 04:23",
+      address:"科教北501-2"
+    },
+    {
+      status:1,
+      roomname:"会议室",
+      usingtime:"2022-12-13 18:00-19:00",
+      ordertime:"2022-12-12 04:23",
+      address:"科教北501-2"
+    }
+  ]
   },
 
   /**
